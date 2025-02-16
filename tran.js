678dd0d0ac5ee8740153430a3bee0c979fa232d3
elfaw775 <2492229698@qq.com>
2025-02-16 20:58:22 +0800

    Update tran.js

--- tran.js
+++ tran.js
@@ -4,8 +4,8 @@ let btnr = document.getElementById('btnr')
 let ask = document.getElementById('ask')
 
 let count = 1
-let imagearray = ["valentine-lil-demo/pic/1.jpg", "valentine-lil-demo/pic/2.jpg", "valentine-lil-demo/pic/3.jpg",
-    "valentine-lil-demo/pic/4.jpg", "valentine-lil-demo/pic/5.jpg",
+let imagearray = ["./pic/1.jpg", "./pic/2.jpg", "./pic/3.jpg",
+    "./pic/4.jpg", "./pic/5.jpg",
 ]
 let par = ["", "? 你认真的吗", "要不再想想", "不许选这个!", "我会很伤心..."]
 let move = 20
@@ -14,7 +14,7 @@ let move = 20
 
 
 btnl.addEventListener('click', function() {
-    pic.src = "valentine-lil-demo/pic/6.jpg";
+    pic.src = "./pic/6.jpg";
     ask.innerHTML = "!!!喜欢你!!(>„<)♡。";
     btnr.style.display = "none";
     btnl.style.display = "none";
